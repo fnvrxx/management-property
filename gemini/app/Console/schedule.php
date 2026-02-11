a79b79ab526aba6a6fe5c9bf6b80a3252e84b40d
Fajar Adie Santosa <94214658+fnvrxx@users.noreply.github.com>
2026-02-12 01:38:41 +0700

    Delete gemini directory

--- gemini/app/Console/schedule.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Schedule;
-
-Schedule::command('invoices:generate')->dailyAt('01:00');
-// Atau:
-// Schedule::command('invoices:generate')->monthlyOn(1, '01:00');
